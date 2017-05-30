9cfc6e109d55056bb24fe571a678c9e1cf14a8b6
Santiago Garces Gonzalez <sgarcesg1@gmail.com>
2017-05-29 22:16:52 -0500

    adding support for CORS

--- src/main/java/co/edu/eafit/dis/olimpo/service/ArtistService.java
+++ src/main/java/co/edu/eafit/dis/olimpo/service/ArtistService.java
@@ -1,5 +1,6 @@
 package co.edu.eafit.dis.olimpo.service;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.bson.types.ObjectId;
@@ -7,6 +8,8 @@ import org.bson.types.ObjectId;
 import co.edu.eafit.dis.olimpo.entity.Artist;
 
 public interface ArtistService {
+	
+	List<Artist> getAllArtist();
 
 	Optional<Artist> getById(ObjectId id);
 
